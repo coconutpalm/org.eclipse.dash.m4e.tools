80a84ffbb723356929479bb12990be36a8cab4a6
Aaron Digulla <digulla@hepe.com>
2011-08-24 12:06:11 +0200

    Last tool: Create a POM with a dependencyManagement element

---
+++ src/main/groovy/m4e/DependencyManagementCmd.groovy
@@ -0,0 +1,124 @@
+/*******************************************************************************
+ * Copyright (c) 24.08.2011 Aaron Digulla.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Aaron Digulla - initial API and implementation and/or initial documentation
+ *******************************************************************************/
+package m4e
+
+import groovy.xml.MarkupBuilder;
+import java.io.File;
+import java.text.SimpleDateFormat;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+class DependencyManagementCmd extends AbstractCommand {
+        
+    static final String DESCRIPTION = '''\
+repository groupId:artifactId:version - Create a POM with a huge dependencyManagement element that contains all the versions of all the artifacts in the repository
+'''
+    
+    File repo
+    File dmPom
+    String groupId
+    String artifactId
+    String version
+
+    void run( String... args ) {
+        if( args.size() == 1 ) {
+            throw new UserError( 'Missing path to repository to analyze' )
+        }
+        
+        repo = new File( args[1] ).canonicalFile
+        if( !repo.exists() ) {
+            throw new UserError( "Directory ${repo} doesn't exist" )
+        }
+
+        if( args.size() <= 2 ) {
+            throw new UserError( 'Missing groupId:artifactId:version of the POM to create' )
+        }
+
+        String key = args[2]
+        
+        dmPom = MavenRepositoryTools.buildPath( repo, key, 'pom' )
+        
+        String[] parts = key.split( ':', -1 )
+        groupId = parts[0]
+        artifactId = parts[1]
+        version = parts[2]
+        
+        createPom()
+    }
+    
+    void createPom() {
+        dmPom.parentFile.makedirs()
+        
+        File tmp = new File( dmPom.path + '.tmp' )
+        
+        tmp.withWriter { Writer writer ->
+            writer << """\
+<project xmlns="http://maven.apache.org/POM/4.0.0" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+        xsi:schemaLocation="http://maven.apache.org/POM/4.0.0 http://maven.apache.org/xsd/maven-4.0.0.xsd">
+    <modelVersion>4.0.0</modelVersion>
+
+    <groupId>${groupId}</groupId>
+    <artifactId>${artifactId}</artifactId>
+    <version>${version}</version>
+    
+    <dependencyManagement>
+        <dependencies>
+"""
+
+            writeDependencies( writer )            
+            
+            writer << """\
+        </dependencies>
+    </dependencyManagement>
+</project>
+"""
+        }
+        
+        if( dmPom.exists() ) {
+            dmPom.usefulDelete()
+        }
+
+        tmp.usefulRename( dmPom )
+    }
+    
+    void writeDependencies( Writer writer ) {
+        MavenRepositoryTools.eachPom( repo ) {
+            if( dmPom != it ) {
+                def pom = Pom.load( it )
+                writeDependency( writer, pom )
+            }
+        }
+    }
+    
+    /** short key -> version */
+    Map<String, String> versions = [:]
+    
+    void writeDependency( Writer writer, Pom pom ) {
+        String version = pom.version()
+        String old = versions.put( pom.shortKey(), version )
+        if( null != old ) {
+            twoVersionsError( pom, old )
+            return
+        }
+        
+        writer << """\
+            <dependency>
+                <groupId>${pom.groupId()}</groupId>
+                <artifactId>${pom.value( Pom.ARTIFACT_ID )}</artifactId>
+                <version>${version}</version>
+            </dependency>
+"""
+    }
+    
+    void twoVersionsError( Pom pom, String oldVersion ) {
+        log.error( "The repository contains two versions of ${pom.shortKey()}: ${pom.version()} and ${oldVersion}. Omitting ${pom.version()}" )
+    }
+}
---
+++ src/test/groovy/m4e/DependencyManagementCmdTest.groovy
@@ -0,0 +1,44 @@
+package m4e;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+import org.slf4j.Logger;
+
+class DependencyManagementCmdTest {
+
+    @Test
+    public void testRepo1() throws Exception {
+        
+        MopSetup.setup()
+        
+        File source = new File( 'data/input/repo1' )
+        File copy = new File( 'tmp/dmRepo' )
+        
+        assert copy.deleteDir()
+        source.copy( copy )
+        
+        DependencyManagementCmd tool = new DependencyManagementCmd()
+        
+        List<String> errors = []
+        def log = [ error: { errors << it } ] as Logger
+        tool.log = log
+        
+        tool.run([ 'dm', copy.path, 'org.eclipse.dash:dependency-management:3.7.0' ])
+        
+        File expectedDmFile = new File( 'tmp/dmRepo/org/eclipse/dash/dependency-management/3.7.0/dependency-management-3.7.0.pom' )
+        assertTrue( "Missing file ${expectedDmFile}", expectedDmFile.exists() )
+        
+        String expected = new File( 'data/expected/dependency-management-3.7.0.pom' ).getText( 'utf-8' ).normalize().trim()
+        String actual = expectedDmFile.getText( 'utf-8' ).normalize().trim()
+        
+        assertEquals( expected, actual )
+        
+        actual = errors.join( '\n' )
+        
+        // the output depends on the ordering of files in the file system
+        // Do some search'n'replace to always get the same order
+        actual = actual.replace( '3.5.0 and 3.6.0. Omitting 3.5.0', '3.6.0 and 3.5.0. Omitting 3.6.0' )
+        
+        assertEquals( 'The repository contains two versions of org.eclipse.core:org.eclipse.core.runtime: 3.6.0 and 3.5.0. Omitting 3.6.0', errors.join( '\n' ) )
+    }
+}
