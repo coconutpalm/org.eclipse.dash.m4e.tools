297134e733d7d65fea71a18f5441ca73963c2ed1
Aaron Digulla <digulla@hepe.com>
2012-04-25 11:47:40 +0200

    Fixed error logging

--- src/main/groovy/m4e/MopSetup.groovy
+++ src/main/groovy/m4e/MopSetup.groovy
@@ -12,37 +12,39 @@ package m4e
 
 import java.util.zip.ZipInputStream;
 import org.codehaus.groovy.runtime.ProcessGroovyMethods;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 class MopSetup {
-    
+
     private static boolean done = false
 
     static void setup() {
         if( done ) {
             return
         }
-        
+
         done = true
-        
+
         mopUnzip()
         mopUntar()
         mopCopy()
-        
+
         mopString()
         mopFile()
     }
-    
+
     static void mopString() {
         String.metaClass.removeStart = { String pattern ->
             String result = delegate
-            
+
             if( pattern && result.startsWith( pattern ) ) {
                 result = result.substring( pattern.size() )
             }
-            
+
             return result
         }
-        
+
         String.metaClass.removeEnd = { String pattern ->
             String result = delegate
 
@@ -55,30 +57,30 @@ class MopSetup {
 
         String.metaClass.substringBeforeLast = { String pattern ->
             String result = delegate
-            
+
             if( pattern ) {
                 int pos = result.lastIndexOf( pattern )
                 if( pos >= 0 ) {
                     return result[0..<pos]
                 }
             }
-            
+
             return result
         }
-        
+
         String.metaClass.substringAfterLast = { String pattern ->
             String result = delegate
-            
+
             if( pattern ) {
                 int pos = result.lastIndexOf( pattern )
                 if( pos >= 0 ) {
                     return result.substring( pos + pattern.size() )
                 }
             }
-            
+
             return ""
         }
-        
+
         String.metaClass.substringBefore = { String pattern ->
             String result = delegate
 
@@ -91,7 +93,7 @@ class MopSetup {
 
             return result
         }
-        
+
         String.metaClass.substringAfter = { String pattern ->
             String result = delegate
 
@@ -104,18 +106,18 @@ class MopSetup {
 
             return result
         }
-        
+
         String.metaClass.endsWithOneOf = { String... patterns ->
             for( String pattern : patterns ) {
                 if( delegate.endsWith( pattern ) ) {
                     return true
                 }
             }
-            
+
             return false
         }
     }
-    
+
     static void mopFile() {
         File.metaClass.makedirs = {
             if( delegate.exists() ) {
@@ -125,109 +127,109 @@ class MopSetup {
                     throw new IOException( "File ${delegate.absolutePath} exists but it's not a directory" )
                 }
             }
-            
+
             if( !delegate.mkdirs() ) {
                 throw new IOException( "Error creating directory ${delegate.absolutePath}" )
             }
         }
-        
+
         File.metaClass.usefulDelete = {
             if( !delegate.exists() ) {
                 return
             }
-            
+
             if( !delegate.delete() ) {
                 if( delegate.isDirectory() ) {
                     throw new IOException( "Error deleting ${delegate.absolutePath}; expected file but was a directory" )
                 }
-                
+
                 throw new IOException( "Unknown error deleting file ${delegate.absolutePath}" )
             }
         }
-        
+
         File.metaClass.usefulRename = { to ->
             if( !delegate.renameTo( to ) ) {
                 throw new IOException( "Unknown error while renaming file ${delegate.absolutePath} to ${to}" )
             }
         }
-        
+
         File.metaClass.normalize = {
             return PathUtils.normalize( delegate )
         }
-        
+
         File.metaClass.pathRelativeTo = { File parent ->
             String file = delegate.canonicalFile.normalize().toString()
             String parentPath = parent.canonicalFile.normalize().toString() + '/'
-            
+
             return file.removeStart( parentPath )
         }
     }
-    
+
     static void mopCopy() {
         File.metaClass.copy = { File target ->
             if( delegate.isDirectory() ) {
                 target.makedirs()
-                
+
                 delegate.eachFile() { src ->
                     String name = src.name
                     File dest = new File( target, name )
-                    
+
                     if( src.isDirectory() ) {
                         dest.makedirs()
                     }
-                    
+
                     src.copy( dest )
                 }
             } else {
                 if( target.exists() ) {
                     target.delete()
                 }
-                
+
                 target.parentFile?.makedirs()
-                
+
                 delegate.withInputStream() { it ->
                     target << it
                 }
             }
         }
     }
-    
+
     static void mopUnzip() {
         File.metaClass.isChildOf = { File parent ->
             File child = delegate.canonicalFile
             parent = parent.canonicalFile
-            
+
             while( child ) {
                 if( child.equals( parent ) ) {
                     return true
                 }
-                
+
                 child = child.parentFile
             }
         }
-        
+
         // Code based on an example from http://grooveek.blogspot.com/2009/09/adding-zipping-and-unzipping.html
         File.metaClass.unzip = { File destDir ->
             destDir = destDir.absoluteFile
-            
+
             def archive = new ZipInputStream( new FileInputStream( delegate ) )
-            
+
             archive.withStream {
                 def entry
                 while( entry = archive.nextEntry ) {
-                    
+
                     def path = new File( destDir, entry.name )
                     if( !path.isChildOf( destDir ) ) {
                         throw new RuntimeException( "ZIP archive contains odd entry '${entry.name}' which would create the file ${path}" )
                     }
-                    
+
                     if( entry.isDirectory() ) {
                         path.makedirs()
                     } else {
                         path.parentFile?.makedirs()
-                        
+
                         def output = new FileOutputStream( path )
-                        
+
                         output.withStream{
                             int len = 0;
                             byte[] buffer = new byte[10240]
@@ -240,53 +242,58 @@ class MopSetup {
             }
         }
     }
-    
+
     static void mopUntar() {
-        File.metaClass.untar = { File destDir, File log ->
+        File.metaClass.untar = { File destDir, File logFile ->
             destDir.makedirs()
-            
+
             String uncompress = ''
             if( delegate.name.endsWith( '.gz' ) ) {
                 uncompress = 'z'
             } else if( delegate.name.endsWith( '.bz2' ) ) {
                 uncompress = 'j'
             }
-            
+
             List args = [
                 'tar', "-x${uncompress}",
                 '--directory', destDir.absolutePath,
                 '--file', delegate.absolutePath,
             ]*.toString()
-            
+
             def builder = new ProcessBuilder( args )
             builder.redirectErrorStream( true )
-            
+
             Process p = builder.start()
-            
+
             OutputStream output = null
-            if( log ) {
-                output = log.newOutputStream()
+            if( logFile ) {
+                output = logFile.newOutputStream()
             } else {
                 output = System.out
             }
-            
+
             try {
                 ProcessGroovyMethods.consumeProcessOutputStream( p, output )
-                
+
                 int rc = p.waitFor()
                 if( rc != 0 ) {
                     String logInfo = ''
-                    if( log ) {
-                        logInfo = ". See log file ${log.absolutePath}"
+                    if( logFile ) {
+                        logInfo = ". See log file ${logFile.absolutePath}"
                     }
                     throw new RuntimeException( "Error unpacking archive with ${args}${logInfo}" )
                 }
             } finally {
-                if( log && output ) {
-                    try { output.close() } catch( Exception e ) { log.warn( 'Error closing stream', e ) }
+                if( logFile && output ) {
+                    try {
+                        output.close()
+                    } catch( Exception e ) {
+                        Logger log = LoggerFactory.getLogger( File.class )
+                        log.warn( 'Error closing stream', e )
+                    }
                 }
             }
         }
     }
-    
+
 }
