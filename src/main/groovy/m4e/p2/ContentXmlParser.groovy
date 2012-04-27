57d34b45655bd2539a6489e0f8cd4d200338346c
Aaron Digulla <digulla@hepe.com>
2012-04-27 10:24:55 +0200

    Added missing copyright headers

--- src/main/groovy/m4e/p2/ContentXmlParser.groovy
+++ src/main/groovy/m4e/p2/ContentXmlParser.groovy
@@ -1,3 +1,14 @@
+/*******************************************************************************
+ * Copyright (c) 23.04.2012 Aaron Digulla.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Aaron Digulla - initial API and implementation and/or initial documentation
+ *******************************************************************************/
+
 package m4e.p2
 
 import groovy.util.Node;
