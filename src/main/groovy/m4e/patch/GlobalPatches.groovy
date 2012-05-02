ce49cec88362ec17b531c45f7cbaee981c0e141b
Aaron Digulla <digulla@hepe.com>
2012-05-02 16:43:58 +0200

    Moved profiles here

--- src/main/groovy/m4e/patch/GlobalPatches.groovy
+++ src/main/groovy/m4e/patch/GlobalPatches.groovy
@@ -12,8 +12,13 @@
 package m4e.patch
 
 import java.util.List;
+import m4e.UserError
 
 class GlobalPatches {
+    
+    String defaultProfile
+    String profile
+    
     List<String> artifactsToDelete = []
     List<QualifierPatch> qualifierPatches = []
     List<String> orbitExclusions = []
@@ -22,11 +27,31 @@ class GlobalPatches {
     boolean renameOrbitBundles
     
     void merge( GlobalPatches other ) {
+        
+        defaultProfile = merge( "defaultProfile", defaultProfile, other.defaultProfile )
+        profile = merge( "profile", profile, other.profile )
+        
         artifactsToDelete.addAll( other.artifactsToDelete )
         qualifierPatches.addAll( other.qualifierPatches )
         orbitExclusions.addAll( other.orbitExclusions )
         deleteClasses.addAll( other.deleteClasses )
         
         renameOrbitBundles |= other.renameOrbitBundles
+    }
+    
+    String merge( String name, String mine, String other ) {
+        if( !mine ) {
+            return other
+        }
+        
+        if( !other ) {
+            return mine
+        }
+        
+        if( mine != other ) {
+            throw new UserError( "Attempt to redefine ${name}" )
+        }
+        
+        return mine
     }
 }
